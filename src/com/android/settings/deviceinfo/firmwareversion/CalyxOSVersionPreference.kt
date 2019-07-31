af00ae37a49515f58c558ed6774466560003dc27
Chirayu Desai <chirayudesai1@gmail.com>
2019-08-01 01:51:37 +0530

    Show CalyxOS version
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I64bffc767cc84f7fab34e5f8fe7b3a8479de55c2

---
+++ src/com/android/settings/deviceinfo/firmwareversion/CalyxOSVersionPreference.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion
+
+import android.content.Context
+import android.os.SystemProperties
+import androidx.preference.Preference
+import com.android.settings.R
+import com.android.settings.contract.TAG_DEVICE_STATE_PREFERENCE
+import com.android.settingslib.metadata.PreferenceMetadata
+import com.android.settingslib.metadata.PreferenceSummaryProvider
+import com.android.settingslib.preference.PreferenceBinding
+
+// LINT.IfChange
+class CalyxOSVersionPreference :
+    PreferenceMetadata, PreferenceSummaryProvider, PreferenceBinding {
+
+    override val key: String
+        get() = "calyx_os_version"
+
+    override val title: Int
+        get() = R.string.calyxos_version
+
+    override val indexable
+        get() = false
+
+    override fun tags(context: Context) = arrayOf(TAG_DEVICE_STATE_PREFERENCE)
+
+    override fun getSummary(context: Context): CharSequence =
+        SystemProperties.get(CALYXOS_VERSION_PROPERTY, context.getString(R.string.unknown))
+
+    override fun bind(preference: Preference, metadata: PreferenceMetadata) {
+        super.bind(preference, metadata)
+        preference.isSelectable = false
+        preference.isCopyingEnabled = true
+    }
+
+    companion object {
+        const val CALYXOS_VERSION_PROPERTY: String = "ro.calyxos.version"
+    }
+}
+// LINT.ThenChange(CalyxOSVersionPreferenceController.java)
