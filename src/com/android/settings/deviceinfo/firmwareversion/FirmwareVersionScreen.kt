1acb46789554f384d2869cbd71c6ac9170bd9172
Chirayu Desai <chirayudesai1@gmail.com>
2019-08-01 01:51:37 +0530

    Show CalyxOS version
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I64bffc767cc84f7fab34e5f8fe7b3a8479de55c2

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -71,6 +71,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
     override fun getPreferenceHierarchy(context: Context, coroutineScope: CoroutineScope) =
         preferenceHierarchy(context) {
             +FirmwareVersionDetailPreference()
+            +CalyxOSVersionPreference()
             +SecurityPatchLevelPreference()
             +MainlineModuleVersionPreference()
             +BasebandVersionPreference()
