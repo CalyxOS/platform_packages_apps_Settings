80c2ad3285a33469c6eec05a1961ad4697e1c2fd
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 15:10:29 -0700

    Settings: Add hotspot setting to allow VPN upstreams
    Change-Id: I34924fe8e60e3d492f219aa6d0ffa54f0bd46e38

--- src/com/android/settings/TetherSettings.java
+++ src/com/android/settings/TetherSettings.java
@@ -52,6 +52,7 @@ import androidx.preference.Preference;
 import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.FeatureFlags;
+import com.android.settings.dashboard.RestrictedDashboardFragment;
 import com.android.settings.datausage.DataSaverBackend;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.wifi.tether.WifiTetherPreferenceController;
@@ -71,7 +72,7 @@ import java.util.concurrent.atomic.AtomicReference;
  * Displays preferences for Tethering.
  */
 @SearchIndexable
-public class TetherSettings extends RestrictedSettingsFragment
+public class TetherSettings extends RestrictedDashboardFragment
         implements DataSaverBackend.Listener {
 
     @VisibleForTesting
@@ -129,6 +130,15 @@ public class TetherSettings extends RestrictedSettingsFragment
     TetheringManager mTm;
 
     @Override
+    protected String getLogTag() {
+        return TetherSettings.class.getSimpleName();
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.tether_prefs;
+    }
+
     public int getMetricsCategory() {
         return SettingsEnums.TETHER;
     }
@@ -148,7 +158,6 @@ public class TetherSettings extends RestrictedSettingsFragment
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.tether_prefs);
         mContext = getContext();
         mDataSaverBackend = new DataSaverBackend(mContext);
         mDataSaverEnabled = mDataSaverBackend.isDataSaverEnabled();
