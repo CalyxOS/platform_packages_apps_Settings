CalyxOS/platform_packages_apps_Settings

commit 4d8d8223f679f65bba50e229646b957b773104c0

Settings: Add hotspot setting to allow VPN upstreams
[mkbestas]: Remove LineageSettings dependency for calyx

Change-Id: I34924fe8e60e3d492f219aa6d0ffa54f0bd46e38

src/com/android/settings/TetherSettings.java
@@ -36,6 +36,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.UserManager;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
@@ -69,6 +70,7 @@ public class TetherSettings extends RestrictedSettingsFragment
     static final String KEY_USB_TETHER_SETTINGS = "usb_tether_settings";
     @VisibleForTesting
     static final String KEY_ENABLE_BLUETOOTH_TETHERING = "enable_bluetooth_tethering";
+    private static final String KEY_TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
     private static final String KEY_DATA_SAVER_FOOTER = "disabled_on_data_saver";
 
     private static final String TAG = "TetheringSettings";
@@ -76,6 +78,8 @@ public class TetherSettings extends RestrictedSettingsFragment
     private SwitchPreference mUsbTether;
 
     private SwitchPreference mBluetoothTether;
+
+    private SwitchPreference mTetheringAllowVpnUpstreams;
 
     private BroadcastReceiver mTetherChangeReceiver;
 
@@ -143,6 +147,8 @@ public class TetherSettings extends RestrictedSettingsFragment
 
         mUsbTether = (SwitchPreference) findPreference(KEY_USB_TETHER_SETTINGS);
         mBluetoothTether = (SwitchPreference) findPreference(KEY_ENABLE_BLUETOOTH_TETHERING);
+        mTetheringAllowVpnUpstreams =
+                (SwitchPreference) findPreference(KEY_TETHERING_ALLOW_VPN_UPSTREAMS);
 
         mDataSaverBackend.addListener(this);
 
@@ -313,6 +319,7 @@ public class TetherSettings extends RestrictedSettingsFragment
             String[] errored) {
         updateUsbState(available, tethered, errored);
         updateBluetoothState();
+        updateTetheringAllowVpnUpstreamsState();
     }
 
     private void updateUsbState(String[] available, String[] tethered,
@@ -376,6 +383,11 @@ public class TetherSettings extends RestrictedSettingsFragment
         }
     }
 
+    private void updateTetheringAllowVpnUpstreamsState() {
+        mBluetoothTether.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS, 0) == 1 ? true : false);
+    }
+
     public static boolean isProvisioningNeededButUnavailable(Context context) {
         return (TetherUtil.isProvisioningNeeded(context)
                 && !isIntentAvailable(context));
@@ -424,6 +436,10 @@ public class TetherSettings extends RestrictedSettingsFragment
             } else {
                 mCm.stopTethering(TETHERING_BLUETOOTH);
             }
+        } else if (preference == mTetheringAllowVpnUpstreams) {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+                    mTetheringAllowVpnUpstreams.isChecked() ? 1 : 0);
         }
 
         return super.onPreferenceTreeClick(preference);

res/values/cm_strings.xml
@@ -28,4 +28,8 @@
 
     <!-- Search keywords -->
     <string name="keywords_notification_log">notifications, notification log</string>
+
+    <!-- Hotspot lineage extras -->
+    <string name="tethering_allow_vpn_upstreams_title">Allow clients to use VPNs</string>
+    <string name="tethering_allow_vpn_upstreams_summary">Permit hotspot clients to use this device\u2019s VPN connections for upstream connectivity</string>
 </resources>

res/xml/tether_prefs.xml
@@ -38,6 +38,11 @@
         android:summary="@string/bluetooth_tethering_subtext"
         settings:keywords="@string/keywords_hotspot_tethering" />
 
+    <SwitchPreference
+        android:key="tethering_allow_vpn_upstreams"
+        android:title="@string/tethering_allow_vpn_upstreams_title"
+        android:summary="@string/tethering_allow_vpn_upstreams_summary" />
+
     <Preference
         android:key="disabled_on_data_saver"
         android:summary="@string/tether_settings_disabled_on_data_saver"

commit c59cd378687c2455b9eef2ec1e6065bfe72b9f3b

TetherSettings: Fix VPN upstream switch preference state
Fixes: 4d8d8223f6 Settings: Add hotspot setting to allow VPN upstreams
Change-Id: I7f79470e544727716c6bdb4e5edc0b4d94269076

src/com/android/settings/TetherSettings.java
@@ -384,7 +384,7 @@ public class TetherSettings extends RestrictedSettingsFragment
     }
 
     private void updateTetheringAllowVpnUpstreamsState() {
-        mBluetoothTether.setChecked(Settings.Secure.getInt(getContentResolver(),
+        mTetheringAllowVpnUpstreams.setChecked(Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS, 0) == 1 ? true : false);
     }
 