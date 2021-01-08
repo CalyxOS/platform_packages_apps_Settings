1c15ba7d89e2322a7980b39ad78d2a0ef8c1cad0
Oliver Scott <olivercscott@gmail.com>
2021-01-08 15:49:07 -0500

    Settings: Add support for global cleartext penalties
    Change-Id: Ie79d435cbd1ddf1c1bfce7e68106f0fdd93e51fa

---
+++ src/com/android/settings/development/CleartextNetworkPolicyPreferenceController.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.development;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.os.StrictMode;
+
+import com.android.settings.core.TogglePreferenceController;
+
+import lineageos.providers.LineageSettings;
+
+public class CleartextNetworkPolicyPreferenceController extends TogglePreferenceController {
+
+    public CleartextNetworkPolicyPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        ConnectivityManager connectivityManager =
+                mContext.getSystemService(ConnectivityManager.class);
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null) {
+            LinkProperties linkProperties = connectivityManager.getLinkProperties(network);
+            if (linkProperties != null) {
+                return isChecked() || linkProperties.isPrivateDnsActive() ? AVAILABLE
+                        : DISABLED_DEPENDENT_SETTING;
+            }
+        }
+        return CONDITIONALLY_UNAVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return LineageSettings.Global.getInt(mContext.getContentResolver(),
+                LineageSettings.Global.CLEARTEXT_NETWORK_POLICY, StrictMode.NETWORK_POLICY_INVALID)
+                != StrictMode.NETWORK_POLICY_INVALID;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        return LineageSettings.Global.putInt(mContext.getContentResolver(),
+                LineageSettings.Global.CLEARTEXT_NETWORK_POLICY,
+                isChecked ? StrictMode.NETWORK_POLICY_REJECT : StrictMode.NETWORK_POLICY_INVALID);
+    }
+}
