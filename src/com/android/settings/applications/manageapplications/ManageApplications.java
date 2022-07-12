e4346a509987aa3e9d5434b88bd52b860fe0f044
t-m-w <tmw-likes-calyx@pm.me>
2022-07-12 17:24:22 +0000

    Add Hidden apps filter and indicator to Settings
    * Add drop-down filter for Hidden apps in Settings > Apps.
    * Show "Hidden" text under Hidden apps in the apps list.
    
    This also requires changes to platform_frameworks_base.
    
    Change-Id: I4010e3d5445e90aa1fd9676dcb81d7c80b4efec3
    (cherry picked from commit 6a6562d029b498c0b33572d3482dd3aaebc4491c)

--- src/com/android/settings/applications/manageapplications/ManageApplications.java
+++ src/com/android/settings/applications/manageapplications/ManageApplications.java
@@ -30,6 +30,7 @@ import static com.android.settings.applications.manageapplications.AppFilterRegi
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_DISABLED;
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_ENABLED;
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_FREQUENT;
+import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_HIDDEN;
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_INSTANT;
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_PERSONAL;
 import static com.android.settings.applications.manageapplications.AppFilterRegistry.FILTER_APPS_POWER_ALLOWLIST;
@@ -1019,6 +1020,13 @@ public class ManageApplications extends InstrumentedFragment
         }
     }
 
+    public void setHasHidden(boolean hasHiddenApps) {
+        if (mListType != LIST_TYPE_MAIN) {
+            return;
+        }
+        mFilterAdapter.setFilterEnabled(FILTER_APPS_HIDDEN, hasHiddenApps);
+    }
+
     private void autoSetCollapsingToolbarLayoutScrolling() {
         final CoordinatorLayout.LayoutParams params =
                 (CoordinatorLayout.LayoutParams) mAppBarLayout.getLayoutParams();
@@ -1599,6 +1607,7 @@ public class ManageApplications extends InstrumentedFragment
 
             mManageApplications.setHasDisabled(mState.haveDisabledApps());
             mManageApplications.setHasInstant(mState.haveInstantApps());
+            mManageApplications.setHasHidden(mState.haveHiddenApps());
         }
 
         @VisibleForTesting
