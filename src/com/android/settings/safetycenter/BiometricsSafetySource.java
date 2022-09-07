f43f3d8176cd678d382a12fee976357f84fdbf01
t-m-w <tmw-likes-calyx@pm.me>
2022-09-07 11:02:35 +0000

    Enable more featureful biometrics settings screen
    * Enable AOSP's combined biometric settings screen for all devices.
      This new section offers toggles for whether biometrics can unlock
      the device or solely be used by apps. It takes the place of
      individual biometric unlock options and acts as a container for
      the face and fingerprint unlock options.
    
    * Update preference and section title, along with other strings,
      to match whatever type of biometric method the device supports,
      if it only supports one.
    
    * Remove the "Ways to unlock" category and the intro text, since
      they target devices with both face and fingerprint unlock, and
      no such devices are supported (or exist) at the moment.
    
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Co-authored-by: Chirayu Desai <chirayu@calyxinstitute.org>
    Issue: calyxos#1165
    Change-Id: Ib188cabb762bd30036682f2e4248597b62e45cb0

--- src/com/android/settings/safetycenter/BiometricsSafetySource.java
+++ src/com/android/settings/safetycenter/BiometricsSafetySource.java
@@ -60,7 +60,7 @@ public final class BiometricsSafetySource {
             final RestrictedLockUtils.EnforcedAdmin disablingAdmin =
                     combinedBiometricStatusUtils.getDisablingAdmin();
             setBiometricSafetySourceData(context,
-                    context.getString(R.string.security_settings_biometric_preference_title),
+                    combinedBiometricStatusUtils.getTitle(),
                     combinedBiometricStatusUtils.getSummary(),
                     biometricNavigationUtils.getBiometricSettingsIntent(context,
                             combinedBiometricStatusUtils.getSettingsClassName(), disablingAdmin,
