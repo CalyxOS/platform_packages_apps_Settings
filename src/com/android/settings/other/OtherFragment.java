c00b145ca3a42c5063be8fcdd11f2c37c891fcde
Aayush Gupta <aayushgupta219@gmail.com>
2022-11-05 07:56:51 +0000

    Copy OEM Unlocking to other options
    Change-Id: I1d27d2e4720ed594b45e89c3c975a17c8de0643c
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- src/com/android/settings/other/OtherFragment.java
+++ src/com/android/settings/other/OtherFragment.java
@@ -18,9 +18,14 @@ package com.android.settings.other;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.Intent;
+import android.os.UserManager;
 
 import com.android.settings.R;
-import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.Utils;
+import com.android.settings.dashboard.RestrictedDashboardFragment;
+import com.android.settings.development.OnActivityResultListener;
+import com.android.settings.development.OemUnlockDialogHost;
 import com.android.settingslib.core.AbstractPreferenceController;
 
 import java.util.ArrayList;
@@ -30,8 +35,15 @@ import java.util.List;
  * Fragment that shows several toggle controls moved outside from
  * development settings
  */
-public class OtherFragment extends DashboardFragment {
+public class OtherFragment extends RestrictedDashboardFragment
+        implements OemUnlockDialogHost {
+
     private static final String TAG = "OtherFrag";
+    private List<AbstractPreferenceController> mPreferenceControllers = new ArrayList<>();
+
+    public OtherFragment() {
+        super(UserManager.DISALLOW_DEBUGGING_FEATURES);
+    }
 
     @Override
     public int getMetricsCategory() {
@@ -46,5 +58,46 @@ public class OtherFragment extends DashboardFragment {
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        if (Utils.isMonkeyRunning()) {
+            mPreferenceControllers = new ArrayList<>();
+            return null;
+        }
+        mPreferenceControllers.add(new OemUnlockPreferenceController(context, getActivity(), this));
+        return mPreferenceControllers;
+    }
+
+    @Override
+    public void onOemUnlockDialogConfirmed() {
+        final OemUnlockPreferenceController controller = use(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockConfirmed();
+    }
+
+    @Override
+    public void onOemUnlockDialogDismissed() {
+        final OemUnlockPreferenceController controller = use(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockDismissed();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        boolean handledResult = false;
+        for (AbstractPreferenceController controller : mPreferenceControllers) {
+            if (controller instanceof OnActivityResultListener) {
+                // We do not break early because it is possible for multiple controllers to
+                // handle the same result code.
+                handledResult |=
+                        ((OnActivityResultListener) controller).onActivityResult(
+                                requestCode, resultCode, data);
+            }
+        }
+        if (!handledResult) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
