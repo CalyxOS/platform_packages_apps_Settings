d89e36f827e56990e8f12d566ede63b044f82196
Tommy Webb <tommy@calyxinstitute.org>
2022-11-19 04:59:35 +0000

    Copy Bug Report to other options and protect it
    Copy the Bug Report option from Developer Options and protect it with
    strong authentication, i.e. PIN entry, unless Developer Options is on.
    
    Also includes squashed change:
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Thu Jan 19 11:49:52 2023 -0500
    
        fixup! Copy Bug Report to other options and protect it
    
        RestrictedDashboardFragment is blocked in Safest, DashboardFragment OK
    
        Change-Id: I05545130aa43f97595e45c91d4a65c9f3ad45640
    
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ie62e181f627de8d7ca21e6b76b2d49fbf31f888e

---
+++ src/com/android/settings/other/ProtectedBugreportPreference.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.other;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.settings.BugreportPreference;
+
+/**
+ * A BugreportPreference for which a PreferenceController handles clicks and shows the bug report
+ * dialog manually, allowing access to the dialog to be protected.
+ */
+public class ProtectedBugreportPreference extends BugreportPreference {
+
+    public ProtectedBugreportPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onClick() {
+        // Do nothing; showDialog() must be called.
+    }
+
+    public void showDialog() {
+        super.onClick();
+    }
+}
