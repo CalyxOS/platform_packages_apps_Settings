520d2c2779377cff25ec90489f317e00cfda18cf
Michael Bestas <mkbestas@lineageos.org>
2023-03-20 02:40:07 +0200

    Hide SFPS wakeup setting when it's not supported
    Change-Id: I22d4eb745379a6ca56b8fe23e1584131c4e945a6

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -175,7 +175,8 @@ public class FingerprintSettings extends SubSettings {
             if (manager == null || !manager.isHardwareDetected()) {
                 return null;
             }
-            if (manager.isPowerbuttonFps()) {
+            if (manager.isPowerbuttonFps() && context.getResources().getBoolean(
+                    org.lineageos.platform.internal.R.bool.config_fingerprintWakeAndUnlock)) {
                 controllers.add(
                         new FingerprintUnlockCategoryController(
                                 context,
@@ -244,6 +245,7 @@ public class FingerprintSettings extends SubSettings {
         private PreferenceCategory mFingerprintsEnrolledCategory;
         private PreferenceCategory mFingerprintUnlockCategory;
         private PreferenceCategory mFingerprintUnlockFooter;
+        private boolean mFingerprintWakeAndUnlock;
 
         private FingerprintManager mFingerprintManager;
         private FingerprintUpdater mFingerprintUpdater;
@@ -328,7 +330,7 @@ public class FingerprintSettings extends SubSettings {
                     case MSG_REFRESH_FINGERPRINT_TEMPLATES:
                         removeFingerprintPreference(msg.arg1);
                         updateAddPreference();
-                        if (isSfps()) {
+                        if (isSfps() && mFingerprintWakeAndUnlock) {
                             updateFingerprintUnlockCategoryVisibility();
                         }
                         updatePreferences();
@@ -418,6 +420,8 @@ public class FingerprintSettings extends SubSettings {
             mFingerprintManager = Utils.getFingerprintManagerOrNull(activity);
             mFingerprintUpdater = new FingerprintUpdater(activity, mFingerprintManager);
             mSensorProperties = mFingerprintManager.getSensorPropertiesInternal();
+            mFingerprintWakeAndUnlock = getContext().getResources().getBoolean(
+                    org.lineageos.platform.internal.R.bool.config_fingerprintWakeAndUnlock);
 
             mToken = getIntent().getByteArrayExtra(
                     ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN);
@@ -511,7 +515,7 @@ public class FingerprintSettings extends SubSettings {
                 column2.mTitle = getText(
                         R.string.security_fingerprint_disclaimer_lockscreen_disabled_2
                 );
-                if (isSfps()) {
+                if (isSfps() && mFingerprintWakeAndUnlock) {
                     column2.mLearnMoreOverrideText = getText(
                             R.string.security_settings_fingerprint_settings_footer_learn_more);
                 }
@@ -590,7 +594,7 @@ public class FingerprintSettings extends SubSettings {
             // This needs to be after setting ids, otherwise
             // |mRequireScreenOnToAuthPreferenceController.isChecked| is always checking the primary
             // user instead of the user with |mUserId|.
-            if (isSfps()) {
+            if (isSfps() && mFingerprintWakeAndUnlock) {
                 scrollToPreference(fpPrefKey);
                 addFingerprintUnlockCategory();
             }
@@ -640,7 +644,9 @@ public class FingerprintSettings extends SubSettings {
 
         private void addFingerprintUnlockCategory() {
             mFingerprintUnlockCategory = findPreference(KEY_FINGERPRINT_UNLOCK_CATEGORY);
-            setupFingerprintUnlockCategoryPreferences();
+            if (mRequireScreenOnToAuthPreferenceController != null) {
+                setupFingerprintUnlockCategoryPreferences();
+            }
             final Preference restToUnlockPreference = FeatureFactory.getFeatureFactory()
                     .getFingerprintFeatureProvider()
                     .getSfpsRestToUnlockFeature(getContext())
@@ -654,7 +660,9 @@ public class FingerprintSettings extends SubSettings {
                 mRequireScreenOnToAuthPreference.setOnPreferenceChangeListener(
                         restToUnlockPreference.getOnPreferenceChangeListener());
             }
-            updateFingerprintUnlockCategoryVisibility();
+            if (mFingerprintUnlockCategoryPreferenceController != null) {
+                updateFingerprintUnlockCategoryVisibility();
+            }
         }
 
         private void updateFingerprintUnlockCategoryVisibility() {
@@ -909,7 +917,8 @@ public class FingerprintSettings extends SubSettings {
         private List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
             final List<AbstractPreferenceController> controllers =
                     createThePreferenceControllers(context);
-            if (isSfps()) {
+            if (isSfps() && context.getResources().getBoolean(
+                    org.lineageos.platform.internal.R.bool.config_fingerprintWakeAndUnlock)) {
                 for (AbstractPreferenceController controller : controllers) {
                     if (controller.getPreferenceKey() == KEY_FINGERPRINT_UNLOCK_CATEGORY) {
                         mFingerprintUnlockCategoryPreferenceController =
