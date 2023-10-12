437db5b7e67af2f09061cda3dfeff68ab9495cfe
Oliver Scott <olivercscott@gmail.com>
2023-10-12 16:23:20 -0400

    Hide app feature
    Allow users to uninstall apps while preserving data.
    All apps can be "un-hidden" by updating/reinstalling them. Otherwise for:
     * System apps
       - Personal profile: Settings -> Apps -> All Apps -> Show system ->
         Select app -> Unhide
       - Work profile: Settings -> System -> Multiple Users ->
         Select profile -> App & content access -> Select app -> Unhide
     * Third-party apps:
       - Personal profile: Settings -> Apps -> All Apps ->
         Select app -> Unhide
       - Work profile: Settings -> System -> Multiple Users ->
         Select profile -> App & content access -> Select app -> Unhide
    
    * Added drop-down filter for hidden apps in Settings > Apps.
    * Shows a warning when hiding/unhiding the app.
    
    For further improvements, consider adding more restrictions on top of
    https://developer.android.com/training/package-visibility
    
    Issue: calyxos#623
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Co-authored-by: Tommy Webb <tmw-likes-calyx@pm.me>
    Change-Id: I79d2d4e1e1f5f07d70d625d21ace3e4606165c4f

--- src/com/android/settings/spa/app/AllAppList.kt
+++ src/com/android/settings/spa/app/AllAppList.kt
@@ -97,10 +97,12 @@ class AllAppListModel(
 
     override fun getSpinnerOptions(recordList: List<AppRecordWithSize>): List<SpinnerOption> {
         val hasDisabled = recordList.any(isDisabled)
+        val hasHidden = recordList.any(isHidden)
         val hasInstant = recordList.any(isInstant)
-        if (!hasDisabled && !hasInstant) return emptyList()
+        if (!hasDisabled && !hasInstant && !hasHidden) return emptyList()
         val options = mutableListOf(SpinnerItem.All, SpinnerItem.Enabled)
         if (hasDisabled) options += SpinnerItem.Disabled
+        if (hasHidden) options += SpinnerItem.Hidden
         if (hasInstant) options += SpinnerItem.Instant
         return options.map {
             SpinnerOption(
@@ -121,6 +123,7 @@ class AllAppListModel(
         when (SpinnerItem.entries.getOrNull(option)) {
             SpinnerItem.Enabled -> ({ it.app.enabled && !it.app.isInstantApp })
             SpinnerItem.Disabled -> isDisabled
+            SpinnerItem.Hidden -> isHidden
             SpinnerItem.Instant -> isInstant
             else -> ({ true })
         }
@@ -128,6 +131,9 @@ class AllAppListModel(
 
     private val isDisabled: (AppRecordWithSize) -> Boolean =
         { !it.app.enabled && !it.app.isInstantApp }
+
+    private val isHidden: (AppRecordWithSize) -> Boolean =
+        { (it.app.privateFlags and ApplicationInfo.PRIVATE_FLAG_HIDDEN) != 0 }
 
     private val isInstant: (AppRecordWithSize) -> Boolean = { it.app.isInstantApp }
 
@@ -141,6 +147,10 @@ class AllAppListModel(
                 summaryList += storageSummaryValue
             }
             when {
+                isHidden(record) -> {
+                    summaryList += context.getString(R.string.hidden)
+                }
+
                 !android.multiuser.Flags.dontShowOtherUsersAppsToAdmin() && !record.app.installed && !record.app.isArchived -> {
                     summaryList += context.getString(R.string.not_installed)
                 }
@@ -174,4 +184,5 @@ private enum class SpinnerItem(val stringResId: Int) {
     Enabled(R.string.filter_enabled_apps),
     Disabled(R.string.filter_apps_disabled),
     Instant(R.string.filter_instant_apps),
+    Hidden(R.string.filter_hidden_apps),
 }
