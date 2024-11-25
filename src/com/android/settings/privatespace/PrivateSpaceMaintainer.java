ec81f7c519fe8604ef708b8fc95ae1597417ab8f
Oliver Scott <olivercscott@gmail.com>
2024-11-25 18:01:53 -0500

    Install F-Droid Basic in private space by default
    Issue: calyxos#2769
    Change-Id: I652e5b0314187b060cf06b84b3f8eb06a65a3393

--- src/com/android/settings/privatespace/PrivateSpaceMaintainer.java
+++ src/com/android/settings/privatespace/PrivateSpaceMaintainer.java
@@ -133,6 +133,7 @@ public class PrivateSpaceMaintainer {
             setUserSetupComplete();
             setSkipFirstUseHints();
             disableComponentsToHidePrivateSpaceSettings();
+            installAppStore();
         }
         return true;
     }
@@ -392,6 +393,15 @@ public class PrivateSpaceMaintainer {
         return android.os.Flags.allowPrivateProfile()
                 && android.multiuser.Flags.supportAutolockForPrivateSpace()
                 && android.multiuser.Flags.enablePrivateSpaceFeatures();
+    }
+
+    private void installAppStore() {
+        Context privateSpaceUserContext = mContext.createContextAsUser(mUserHandle, /* flags */ 0);
+        PackageManager packageManager = privateSpaceUserContext.getPackageManager();
+
+        Log.d(TAG, "Installing app store for " + mUserHandle);
+        packageManager.getPackageInstaller().installExistingPackage("org.fdroid.basic",
+                PackageManager.INSTALL_REASON_USER, null);
     }
 
     /**
