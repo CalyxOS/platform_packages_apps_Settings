f559d43658924cd7964f1b6b4ac0da9d04a5e73e
Oliver Scott <olivercscott@gmail.com>
2025-04-03 14:15:32 -0400

    Protect sensitive data in mobile network settings
    Tap to show IMEI, EID and phone number
    
    Change-Id: I31f4bafb772949073c9f228d15c8e7628681f753

--- src/com/android/settings/network/telephony/MobileNetworkImeiPreferenceController.kt
+++ src/com/android/settings/network/telephony/MobileNetworkImeiPreferenceController.kt
@@ -107,7 +107,6 @@ class MobileNetworkImeiPreferenceController(context: Context, key: String) :
             imei = getImei()
             simSlot = subscription.simSlotIndex
         }
-        refreshUi()
     }
 
     private fun refreshUi(){
@@ -120,6 +119,7 @@ class MobileNetworkImeiPreferenceController(context: Context, key: String) :
 
         Log.d(TAG, "handlePreferenceTreeClick:")
         ImeiInfoDialogFragment.show(fragment, simSlot, preference.title.toString())
+        refreshUi()
         return true
     }
 
@@ -143,6 +143,10 @@ class MobileNetworkImeiPreferenceController(context: Context, key: String) :
         val phoneType = getPhoneType()
         return if (phoneType == TelephonyManager.PHONE_TYPE_CDMA) getTitleForCdmaPhone()
                 else getTitleForGsmPhone()
+    }
+
+    override fun getSummary(): CharSequence {
+        return mContext.getString(R.string.device_info_protected_single_press)
     }
 
     /**
--- src/com/android/settings/network/telephony/MobileNetworkPhoneNumberPreferenceController.kt
+++ src/com/android/settings/network/telephony/MobileNetworkPhoneNumberPreferenceController.kt
@@ -36,6 +36,7 @@ constructor(
 ) : TelephonyBasePreferenceController(context, key) {
 
     private lateinit var preference: Preference
+    private var phoneNumber = String()
 
     fun init(subId: Int) {
         mSubId = subId
@@ -57,8 +58,18 @@ constructor(
     override fun onViewCreated(viewLifecycleOwner: LifecycleOwner) {
         subscriptionRepository.phoneNumberFlow(mSubId).collectLatestWithLifecycle(
             viewLifecycleOwner) { phoneNumber ->
-                preference.summary = phoneNumber ?: getStringUnknown()
+                this.phoneNumber = phoneNumber ?: getStringUnknown()
             }
+    }
+
+    override fun getSummary(): CharSequence {
+        return mContext.getString(R.string.device_info_protected_single_press)
+    }
+
+    override fun handlePreferenceTreeClick(preference: Preference): Boolean {
+        if (preference.key != preferenceKey) return false
+        preference.summary = phoneNumber
+        return true
     }
 
     private fun getStringUnknown(): String {
