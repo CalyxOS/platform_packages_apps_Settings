71bbb53325c4b88ff2c18704334a7238c68a62fe
Tommy Webb <tommy@calyxinstitute.org>
2025-04-15 20:22:12 +0000

    Notify that global cleartext restriction is removed if enabled
    If global cleartext restriction was enabled, notify that it has been
    removed. We already warned previously, so this alters the warning
    to become a new and final notification confirming that it is now
    removed.
    
    Additionally, this notification only shows for the device's main user
    so that it won't show for every new guest or user created in the future,
    which would happen unless we removed this deleted setting from the
    database, and also so it won't be unnecessarily duplicated for work
    or private profiles.
    
    Issue: calyxos#2987
    Change-Id: I471dc92998af463be428881c6d203eec4e246602

--- src/com/android/settings/development/CleartextNetworkPolicyRemovedBootReceiver.java
+++ src/com/android/settings/development/CleartextNetworkPolicyRemovedBootReceiver.java
@@ -10,32 +10,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.UserManager;
 
 import com.android.settings.R;
 
 import lineageos.providers.LineageSettings;
 
-public class CleartextNetworkPolicyBootReceiver extends BroadcastReceiver {
+public class CleartextNetworkPolicyRemovedBootReceiver extends BroadcastReceiver {
 
     private static final int oneShotUpdateInfoNotificationID = 100;
     private static final String UPDATE_INFO_URL = "https://calyxos.org/global-no-cleartext";
     private static final String updateInfoChannelID = "updateInfo";
+    private static final String CLEARTEXT_NETWORK_POLICY = "cleartext_network_policy";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (LineageSettings.Global.getInt(context.getContentResolver(),
-                LineageSettings.Global.CLEARTEXT_NETWORK_POLICY, -1) != -1) {
+        final boolean isMainUser = UserManager.get(context).isMainUser();
+        if (isMainUser && LineageSettings.Global.getInt(context.getContentResolver(),
+                CLEARTEXT_NETWORK_POLICY, -1) != -1) {
             intent = new Intent(Intent.ACTION_VIEW, Uri.parse(UPDATE_INFO_URL));
 
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent,
                     PendingIntent.FLAG_IMMUTABLE);
 
-            String longDescription = context.getString(R.string.update_info_desc_long);
+            String longDescription =
+                    context.getString(R.string.update_info_cleartext_removal_desc_long);
 
             Notification notification = new Notification.Builder(context, updateInfoChannelID)
                     .setSmallIcon(R.drawable.ic_android)
                     .setContentTitle(context.getString(R.string.update_info_title))
-                    .setContentText(context.getString(R.string.update_info_desc))
+                    .setContentText(context.getString(R.string.update_info_cleartext_removal_desc))
                     .setStyle(new Notification.BigTextStyle().bigText(longDescription))
                     .setContentIntent(contentIntent)
                     .setCategory(Notification.CATEGORY_SYSTEM)
@@ -51,12 +55,12 @@ public class CleartextNetworkPolicyBootReceiver extends BroadcastReceiver {
                     NotificationManager.IMPORTANCE_DEFAULT
             );
             notificationChannel.setDescription(
-                    context.getString(R.string.update_info_channel_desc));
+                    context.getString(R.string.update_info_channel_desc2));
             notificationManager.createNotificationChannel(notificationChannel);
             notificationManager.notify(oneShotUpdateInfoNotificationID, notification);
-            context.getPackageManager().setComponentEnabledSetting(
-                    new ComponentName(context, CleartextNetworkPolicyBootReceiver.class),
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
         }
+        context.getPackageManager().setComponentEnabledSetting(
+                new ComponentName(context, CleartextNetworkPolicyRemovedBootReceiver.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
 }
