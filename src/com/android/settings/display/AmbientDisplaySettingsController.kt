32ea83e39eabd34d2f09fe3da0b67eceb9d67efa
Michael Bestas <mkbestas@gmail.com>
2026-06-18 02:33:06 +0300

    fixup! Settings: Promote external doze settings to first class
    Change-Id: I447f0cdb1067a3466409ca5b39bb9651107044ca

--- src/com/android/settings/display/AmbientDisplaySettingsController.kt
+++ src/com/android/settings/display/AmbientDisplaySettingsController.kt
@@ -12,7 +12,6 @@ import android.provider.Settings
 import android.provider.Settings.Secure.DOZE_ENABLED
 import com.android.settings.R
 import com.android.settings.core.TogglePreferenceController
-import com.android.systemui.shared.Flags.ambientAod
 
 class AmbientDisplaySettingsController(
     context: Context,
@@ -20,12 +19,7 @@ class AmbientDisplaySettingsController(
 ) : TogglePreferenceController(context, key) {
 
     override fun getAvailabilityStatus(): Int =
-        AVAILABLE.takeIf  {
-            when (preferenceKey) {
-                "ambient_display_external_settings" -> ambientAod()
-                else -> !ambientAod()
-            } && isExternallyManaged(mContext)
-        } ?: UNSUPPORTED_ON_DEVICE
+        if (isExternallyManaged(mContext)) AVAILABLE else UNSUPPORTED_ON_DEVICE
 
     override fun getSliceHighlightMenuRes(): Int = R.string.menu_key_display
 
