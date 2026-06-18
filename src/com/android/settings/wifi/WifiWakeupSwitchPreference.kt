7ea632e05aefab7a62b747140522529c2e443d9a
Michael Bestas <mkbestas@gmail.com>
2026-06-18 07:50:25 +0300

    fixup! Restrict "Turn on wifi automatically" to USA and Canada
    Change-Id: I21031eb36fa8ed75b1641c070c55619905c6a5a4

--- src/com/android/settings/wifi/WifiWakeupSwitchPreference.kt
+++ src/com/android/settings/wifi/WifiWakeupSwitchPreference.kt
@@ -43,6 +43,7 @@ import com.android.settingslib.metadata.PreferenceSummaryProvider
 import com.android.settingslib.metadata.ReadWritePermit
 import com.android.settingslib.metadata.SensitivityLevel
 import com.android.settingslib.metadata.SwitchPreference
+import com.android.settingslib.metadata.preferencesapi.preconditions.PreconditionStability
 import com.android.settingslib.preference.SwitchPreferenceBinding
 
 @Suppress("DEPRECATION")
@@ -192,6 +193,10 @@ class WifiWakeupSwitchPreference :
     }
 
     override fun getEnabledDescription(): String = "Requires Location to be enabled. Requires Wi-Fi scanning to be always available."
+
+    override val availabilityDescription = "Country specific feature."
+
+    override fun getAvailabilityStability() = PreconditionStability.UNSTABLE
 
     override fun isAvailable(context: Context): Boolean {
         return context.getResources().getBoolean(R.bool.config_wifi_wakeup)
